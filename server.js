e5c1b715d90abce30c6cbfe88e24c7524d0a9ca3
Stefano <ste.zullo@icloud.com>
2019-08-26 21:36:23 +0100

    her works

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/food-recognizer-fe'));
+app.use(express.static(__dirname + '/dist/foodRecognizerFE'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/food-recognizer-fe/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/foodRecognizerFE/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
