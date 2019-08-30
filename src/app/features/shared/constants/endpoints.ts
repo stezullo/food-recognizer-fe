e27614cc46ebbb48262504d6da239c5c35ece1ff
Stefano <ste.zullo@icloud.com>
2019-08-30 14:13:54 +0100

    fix heroku

--- src/app/features/shared/constants/endpoints.ts
+++ src/app/features/shared/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export default class Endpoints {
-    private static readonly BASE_URL: string = "http://localhost:3000";
-    //private static readonly BASE_URL: string = "https://food-recognizer-be.herokuapp.com";
+    //private static readonly BASE_URL: string = "http://localhost:3000";
+    private static readonly BASE_URL: string = "https://food-recognizer-be.herokuapp.com";
     static readonly IMAGE_RECOGNITION: string = Endpoints.BASE_URL + "/recognition";
     static readonly LOGIN: string = Endpoints.BASE_URL + "/auth/login";
 }
