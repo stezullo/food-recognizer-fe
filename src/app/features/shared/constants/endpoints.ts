fd75679de73e5e4e15313f9e12c8de84b0cedb25
Stefano <ste.zullo@icloud.com>
2019-09-05 12:49:32 +0100

    endpoints automation

--- src/app/features/shared/constants/endpoints.ts
+++ src/app/features/shared/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export default class Endpoints {
-    //private static readonly BASE_URL: string = "http://localhost:3000";
-    private static readonly BASE_URL: string = "https://food-recognizer-be.herokuapp.com";
+    private static readonly HEROKU_ENABLED: boolean = true;
+    private static readonly BASE_URL: string = (Endpoints.HEROKU_ENABLED) ? "https://food-recognizer-be.herokuapp.com" : "http://localhost:3000";
     static readonly IMAGE_RECOGNITION: string = Endpoints.BASE_URL + "/recognition";
     static readonly LOGIN: string = Endpoints.BASE_URL + "/auth/login";
 }
